e186319b0935fc4fa825b2780b034e9198b977fc
KeithLin724 <keithlin724@gmail.com>
2025-03-28 23:54:16 +0800

    add some plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -208,4 +208,19 @@ return {
       },
     },
   },
+  -- about the conda
+  {
+    'kmontocam/nvim-conda',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  -- about the render markdown
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 }
